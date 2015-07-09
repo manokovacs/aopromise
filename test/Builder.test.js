4a6e3a425a28863efd3ee71ffbeb13047d193c98
Mano Kovacs <munaw@munaw.com>
2015-07-09 22:55:27 +0200

    Builder and tests

--- test/Builder.test.js
+++ test/Builder.test.js
@@ -24,4 +24,49 @@ describe('Builder', function () {
 
 			});
 	});
+
+	it('should allow to use registered aspect', function (end) {
+		var pre1 = sinon.spy();
+		var pre2 = sinon.spy();
+		Builder.register('aspect1', function () {
+			return new AspectFrame(pre1);
+		});
+		Builder.register('aspect2', function () {
+			return new AspectFrame(pre2);
+		});
+
+		var builder = new Builder(function (fn, aspectPack) {
+			aspectPack.pre({})
+				.then(function () {
+					pre1.calledOnce.should.be.true();
+					pre2.calledOnce.should.be.true();
+					end();
+				}).catch(end);
+		});
+		builder
+			.aspect1()
+			.aspect2()
+			.fn(function () {
+
+			});
+		Builder.unregister('aspect1');
+		Builder.unregister('aspect2');
+	});
+
+	it('should remove registered call aspect', function (end) {
+		var pre1 = sinon.spy();
+		Builder.register('aspect1', function () {
+			return new AspectFrame(pre1);
+		});
+
+		new Builder().aspect1();
+		Builder.unregister('aspect1');
+
+		try {
+			new Builder().aspect1();
+			end(new Error('aspect1 should be have been removed'));
+		} catch (err) {
+			end();
+		}
+	});
 });
